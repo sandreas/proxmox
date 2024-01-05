75ce6795c2242c541dfd225250b4f649caa2e39e
sandreas <2050604+sandreas@users.noreply.github.com>
2024-01-06 00:21:56 +0100

    added todo

--- 02-basic-setup.sh
+++ 02-basic-setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# todo
+# - zfs-auto-snapshot - see https://raw.githubusercontent.com/extremeshok/xshok-proxmox/master/install-post.sh
+# - docker lxc - see https://tteck.github.io/Proxmox/ (docker)
+# - create "shared" user
+# - configure portainer
+# - configure pi-hole
+# - configure immich
+
 # update lists
 apt update
 
@@ -31,4 +39,29 @@ dpkg -s powertop &> /dev/null || apt -y install powertop
 # maybe powertop --calibrate and a reboot is required followed by another powertop --calibrate
 powertop --auto-tune &> /dev/null
 
-
+# zfs-auto-snapshot
+if [ "${XS_ZFSAUTOSNAPSHOT,,}" == "yes" ] ; then
+    ## Install zfs-auto-snapshot
+    /usr/bin/env DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' install zfs-auto-snapshot
+    # make 5min snapshots , keep 12 5min snapshots
+    if [ -f "/etc/cron.d/zfs-auto-snapshot" ] ; then
+      sed -i 's|--keep=[0-9]*|--keep=12|g' /etc/cron.d/zfs-auto-snapshot
+      sed -i 's|*/[0-9]*|*/5|g' /etc/cron.d/zfs-auto-snapshot
+    fi
+    # keep 24 hourly snapshots
+    if [ -f "/etc/cron.hourly/zfs-auto-snapshot" ] ; then
+      sed -i 's|--keep=[0-9]*|--keep=24|g' /etc/cron.hourly/zfs-auto-snapshot
+    fi
+    # keep 7 daily snapshots
+    if [ -f "/etc/cron.daily/zfs-auto-snapshot" ] ; then
+      sed -i 's|--keep=[0-9]*|--keep=7|g' /etc/cron.daily/zfs-auto-snapshot
+    fi
+    # keep 4 weekly snapshots
+    if [ -f "/etc/cron.weekly/zfs-auto-snapshot" ] ; then
+      sed -i 's|--keep=[0-9]*|--keep=4|g' /etc/cron.weekly/zfs-auto-snapshot
+    fi
+    # keep 3 monthly snapshots
+    if [ -f "/etc/cron.monthly/zfs-auto-snapshot" ] ; then
+      sed -i 's|--keep=[0-9]*|--keep=3|g' /etc/cron.monthly/zfs-auto-snapshot
+    fi
+fi
